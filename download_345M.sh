b6b5a11d1e8f29380bb8d5840eec870f225a6eb4
BlackDChase <blackdragonchase@gmail.com>
2020-01-22 06:07:20 +0530

    Moving other important files inside gpt-2

--- download_345M.sh
+++ download_345M.sh
@@ -5,5 +5,8 @@ cd gpt-2
 pip3 install -r requirements.txt # install required modules
 python3 download_model.sh 345M # download original OA model, medium
 mv ../finetune.sh ./
-./finetune.sh
-../dataset/clean_fantasy/
+mv ../conditional_genration.sh ./
+mv ../save_last_trained_model.sh ./
+#./finetune.sh
+#../dataset/clean_fantasy/
+#this was to take input from clean_fantasy, because ../dataset/ added as relative $dataset_loc
